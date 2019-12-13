6ccf698f10b64c6bc40f033e45068887b03e5196
Barry Earsman <barry@earsman.com>
2019-12-13 12:47:48 +1000

    Added new response messages for registering organization user

--- messaging/responses/subjects/organization.go
+++ messaging/responses/subjects/organization.go
@@ -1,13 +1,15 @@
 package subjects
 
 const (
-	ORGANIZATION_CREATE_FAILED      = "bpaas.organization.create.failed"
-	ORGANIZATION_CREATE_IN_PROGRESS = "bpaas.organization.create.inprogress"
-	ORGANIZATION_GET_COMPLETED      = "bpaas.organization.get.completed"
-	ORGANIZATION_LIST_ALL_COMPLETED = "bpaas.organization.list_all.completed"
-	ORGANIZATION_LIST_ALL_FAILED    = "bpaas.organization.list_all.failed"
-	ORGANIZATION_LIST_COMPLETED     = "bpaas.organization.list.completed"
-	ORGANIZATION_LIST_FAILED        = "bpaas.organization.list.failed"
-	ORGANIZATION_STATUS_COMPLETED   = "bpaas.organization.status.completed"
-	ORGANIZATION_STATUS_FAILED      = "bpaas.organization.status.failed"
+	ORGANIZATION_CREATE_FAILED           = "bpaas.organization.create.failed"
+	ORGANIZATION_CREATE_IN_PROGRESS      = "bpaas.organization.create.inprogress"
+	ORGANIZATION_GET_COMPLETED           = "bpaas.organization.get.completed"
+	ORGANIZATION_LIST_ALL_COMPLETED      = "bpaas.organization.list_all.completed"
+	ORGANIZATION_LIST_ALL_FAILED         = "bpaas.organization.list_all.failed"
+	ORGANIZATION_LIST_COMPLETED          = "bpaas.organization.list.completed"
+	ORGANIZATION_LIST_FAILED             = "bpaas.organization.list.failed"
+	ORGANIZATION_STATUS_COMPLETED        = "bpaas.organization.status.completed"
+	ORGANIZATION_STATUS_FAILED           = "bpaas.organization.status.failed"
+	ORGANIZATION_REGISTER_USER_COMPLETED = "bpaas.organization.register_user.completed"
+	ORGANIZATION_REGISTER_USER_FAILED    = "bpaas.organization.register_user.failed"
 )
