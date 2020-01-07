609d9e42e63a32e0e4dd57147de89c1759722aaf
Barry Earsman <barry@earsman.com>
2020-01-07 13:31:12 +1000

    Notification messages separated out

--- messaging/responses/subjects/invite.go
+++ messaging/responses/subjects/invite.go
@@ -1,15 +1,12 @@
 package subjects
 
 const (
-	INVITE_ACCEPT_COMPLETED    = "bpaas.invite.accept.completed"
-	INVITE_ACCEPT_FAILED       = "bpaas.invite.accept.failed"
-	INVITE_LIST_COMPLETED      = "bpaas.invite.list.completed"
-	INVITE_LIST_FAILED         = "bpaas.invite.list.failed"
-	INVITE_REJECT_COMPLETED    = "bpaas.invite.reject.completed"
-	INVITE_REJECT_FAILED       = "bpaas.invite.reject.failed"
-	INVITE_SEND_COMPLETED      = "bpaas.invite.send.completed"
-	INVITE_SEND_FAILED         = "bpaas.invite.send.failed"
-	NOTIFICATION_INVITE_ACCEPT = "bpaas.notification.invite.accept"
-	NOTIFICATION_INVITE        = "bpaas.notification.invite"
-	NOTIFICATION_REJECT        = "bpaas.notification.invite.reject"
+	INVITE_ACCEPT_COMPLETED = "bpaas.invite.accept.completed"
+	INVITE_ACCEPT_FAILED    = "bpaas.invite.accept.failed"
+	INVITE_LIST_COMPLETED   = "bpaas.invite.list.completed"
+	INVITE_LIST_FAILED      = "bpaas.invite.list.failed"
+	INVITE_REJECT_COMPLETED = "bpaas.invite.reject.completed"
+	INVITE_REJECT_FAILED    = "bpaas.invite.reject.failed"
+	INVITE_SEND_COMPLETED   = "bpaas.invite.send.completed"
+	INVITE_SEND_FAILED      = "bpaas.invite.send.failed"
 )
