871e945748af41d8fa2f2437e2e63723d0ee2209
Barry Earsman <barry@earsman.com>
2020-01-07 11:45:32 +1000

    Notification messages

--- messaging/responses/subjects/notification.go
+++ messaging/responses/subjects/notification.go
@@ -1,3 +1,7 @@
 package subjects
 
-const ()
+const (
+	NOTIFICATION_INVITE_ACCEPT = "bpaas.notification.invite.accept"
+	NOTIFICATION_INVITE        = "bpaas.notification.invite"
+	NOTIFICATION_REJECT        = "bpaas.notification.invite.reject"
+)
