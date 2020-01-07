fa03aed6d65c02a0cd8ecc7262afc3de8cb7c932
Barry Earsman <barry@earsman.com>
2020-01-07 13:45:48 +1000

    Added new messages: bpaas.chaincode.list_by_channel bpaas.organization.user.register

--- messaging/requests/subjects/chaincode.go
+++ messaging/requests/subjects/chaincode.go
@@ -1,8 +1,9 @@
 package subjects
 
 const (
-	CHAINCODE_LIST         = "bpaas.chaincode.list"
-	CHAINCODE_INSTALL      = "bpaas.chaincode.install"
-	CHAINCODE_INSTANTIATE  = "bpaas.chaincode.instantiate"
-	CHAINCODE_INSTANTIATED = "bpaas.chaincode.instantiated"
+	CHAINCODE_LIST            = "bpaas.chaincode.list"
+	CHAINCODE_LIST_BY_CHWNNEL = "bpaas.chaincode.list_by_channel"
+	CHAINCODE_INSTALL         = "bpaas.chaincode.install"
+	CHAINCODE_INSTANTIATE     = "bpaas.chaincode.instantiate"
+	CHAINCODE_INSTANTIATED    = "bpaas.chaincode.instantiated"
 )
--- messaging/requests/subjects/organization.go
+++ messaging/requests/subjects/organization.go
@@ -1,9 +1,10 @@
 package subjects
 
 const (
-	ORGANIZATION_CREATE   = "bpaas.organization.create"
-	ORGANIZATION_GET      = "bpaas.organization.get"
-	ORGANIZATION_LIST_ALL = "bpaas.organization.list_all"
-	ORGANIZATION_LIST     = "bpaas.organization.list"
-	ORGANIZATION_STATUS   = "bpaas.organization.status"
+	ORGANIZATION_CREATE        = "bpaas.organization.create"
+	ORGANIZATION_GET           = "bpaas.organization.get"
+	ORGANIZATION_LIST_ALL      = "bpaas.organization.list_all"
+	ORGANIZATION_LIST          = "bpaas.organization.list"
+	ORGANIZATION_REGISTER_USER = "bpaas.organization.user.register"
+	ORGANIZATION_STATUS        = "bpaas.organization.status"
 )
