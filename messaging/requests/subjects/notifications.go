77f6076eb3809bbb07a8ad3641e92ad8a4b7a48a
Barry Earsman <barry@earsman.com>
2020-01-30 11:40:13 +1000

    New notification message constants

--- messaging/requests/subjects/notifications.go
+++ messaging/requests/subjects/notifications.go
@@ -1,14 +1,16 @@
 package subjects
 
 const (
-	NOTIFICATION_CHAINCODE_INSTANTIATED = "bpaas.notification.chaincode.instantiated"
-	NOTIFICATION_CONSORTIUM_INVITATION  = "bpaas.notification.consortium_invitation"
-	NOTIFICATION_INVITE                 = "bpaas.notification.invite"
-	NOTIFICATION_INVITE_ACCEPT          = "bpaas.notification.invite.accept"
-	NOTIFICATION_INVITE_REJECT          = "bpaas.notification.invite.reject"
-	NOTIFICATION_PASSWORD_RESET         = "bpaas.notification.user.password_reset"
-	NOTIFICATION_PLATFORM_USER_CREATED  = "bpaas.notification.user.platform.created"
-	NOTIFICATION_USER_ACTIVATED         = "bpaas.notification.user.activated"
-	NOTIFICATION_USER_CREATED           = "bpaas.notification.user.created"
-	NOTIFICATION_USER_SUSPENDED         = "bpaas.notification.user.suspended"
+	NOTIFICATION_CHAINCODE_INSTANTIATED        = "bpaas.notification.chaincode.instantiated"
+	NOTIFICATION_CONSORTIUM_INVITATION         = "bpaas.notification.consortium_invitation"
+	NOTIFICATION_INVITE                        = "bpaas.notification.invite"
+	NOTIFICATION_INVITE_ACCEPT                 = "bpaas.notification.invite.accept"
+	NOTIFICATION_INVITE_REJECT                 = "bpaas.notification.invite.reject"
+	NOTIFICATION_PASSWORD_RESET                = "bpaas.notification.user.password_reset"
+	NOTIFICATION_PLATFORM_USER_CREATED         = "bpaas.notification.user.platform.created"
+	NOTIFICATION_USER_ACTIVATED                = "bpaas.notification.user.activated"
+	NOTIFICATION_USER_CREATED                  = "bpaas.notification.user.created"
+	NOTIFICATION_USER_SUSPENDED                = "bpaas.notification.user.suspended"
+	NOTIFICATION_ORGANIZATION_REQUEST_REJECTED = "bpaas.notification.organization_request_rejected"
+	NOTIFICATION_ORGANIZATION_REQUEST_ACCEPTED = "bpaas.notification.organization_request_accepted"
 )
