82ad7b2f55b60e5f137fb82f02182c0f178213eb
Barry Earsman <barry@earsman.com>
2020-02-21 14:52:29 +1000

    Added bpaas.organization.list_users

--- messaging/requests/subjects/organization.go
+++ messaging/requests/subjects/organization.go
@@ -5,6 +5,7 @@ const (
 	ORGANIZATION_GET           = "bpaas.organization.get"
 	ORGANIZATION_LIST_ALL      = "bpaas.organization.list_all"
 	ORGANIZATION_LIST          = "bpaas.organization.list"
+	ORGANIZATION_LIST_USERS    = "bpaas.organization.list_users"
 	ORGANIZATION_REGISTER_USER = "bpaas.organization.user.register"
 	ORGANIZATION_STATUS        = "bpaas.organization.status"
 )
