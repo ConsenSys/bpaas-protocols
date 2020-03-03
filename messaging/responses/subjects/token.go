e76625ae9b8f7a2bc3dcf2ce983acdbf055d0ac1
Barry Earsman <barry@earsman.com>
2020-03-03 14:34:33 +1000

    Update constants to match master branches

--- messaging/responses/subjects/token.go
+++ messaging/responses/subjects/token.go
@@ -5,6 +5,6 @@ const (
 	CREATE_TOKEN_FAILED    = "bpaas.token.create.failed"
 	TOKEN_EXPIRE_COMPLETED = "bpaas.token.expire.completed"
 	TOKEN_EXPIRE_FAILED    = "bpaas.token.expire.failed"
-	GET_TOKEN_COMPLETED    = "bpaas.token.get.completed"
+	GET_TOKEN_COMPLETE     = "bpaas.token.get.completed"
 	GET_TOKEN_FAILED       = "bpaas.token.get.failed"
 )
