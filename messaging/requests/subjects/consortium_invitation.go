22bdf4dac58073c54e4c474fd5ec3a3cb16cfae3
Barry Earsman <barry@earsman.com>
2020-03-04 12:50:43 +1000

    Changed constants to match master branches

--- messaging/requests/subjects/consortium_invitation.go
+++ messaging/requests/subjects/consortium_invitation.go
@@ -1,12 +1,12 @@
 package subjects
 
 const (
-	CONSORTIUM_INVITATIONS_APPROVAL_ACCEPT = "bpaas.consortium_invitation.approval.accept"
-	CONSORTIUM_INVITATIONS_APPROVAL_REJECT = "bpaas.consortium_invitation.approval.reject"
-	CONSORTIUM_INVITATIONS_ACCEPT          = "bpaas.consortium_invitation.accept"
-	CONSORTIUM_INVITATIONS_APPROVAL_LIST   = "bpaas.consortium_invitation.approval.list"
-	CONSORTIUM_INVITATIONS_CREATE          = "bpaas.consortium_invitation.create"
-	CONSORTIUM_INVITATIONS_GET             = "bpaas.consortium_invitation.get"
-	CONSORTIUM_INVITATIONS_OUTGOING_LIST   = "bpaas.consortium_invitation.outgoing.list"
-	CONSORTIUM_INVITATIONS_REJECT          = "bpaas.consortium_invitation.reject"
+	CONSORTIUM_INVITATIONS_APPROVALS_ACCEPT = "bpaas.consortium_invitation.approval.accept"
+	CONSORTIUM_INVITATIONS_APPROVALS_REJECT = "bpaas.consortium_invitation.approval.reject"
+	CONSORTIUM_INVITATIONS_ACCEPT           = "bpaas.consortium_invitation.accept"
+	CONSORTIUM_INVITATIONS_APPROVAL_LIST    = "bpaas.consortium_invitation.approval.list"
+	CONSORTIUM_INVITATIONS_CREATE           = "bpaas.consortium_invitation.create"
+	CONSORTIUM_INVITATIONS_GET              = "bpaas.consortium_invitation.get"
+	CONSORTIUM_INVITATIONS_OUTGOING_LIST    = "bpaas.consortium_invitation.outgoing.list"
+	CONSORTIUM_INVITATIONS_REJECT           = "bpaas.consortium_invitation.reject"
 )
