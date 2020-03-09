62661da2c47fbcd7be6cce26778fa1f6aface5b8
Barry Earsman <barry@earsman.com>
2020-03-09 16:52:30 +1000

    Removed duplicate definitions

--- messaging/dto/consortium_dto.go
+++ messaging/dto/consortium_dto.go
@@ -1,41 +1,18 @@
 package dto
 
 type ConsortiumDTO struct {
+	ID                         string
+	Name                       string
+	OriginalName               string
+	Description                string
+	OrganizationsCount         uint
+	ChannelsCount              uint
+	ConsortiumInvitationsCount uint
+	Organizations              []*OrganizationDTO
+}
+
+type OrganizationDTO struct {
 	ID           string
 	Name         string
 	OriginalName string
-	Description  string
-}
-
-type ChannelDTO struct {
-	ConsortiumID string
-	Name         string
-	OriginalName string
-	Status       string
-	Public       bool
-}
-
-type InvitationDTO struct {
-	ConsortiumID      string
-	InviterOrgID      string
-	InvitedEmail      string
-	InvitedID         string
-	InitialOrgsCount  uint
-	SelectedPeers     string
-	InvitedOrgID      string
-	ChannelID         string
-	ApprovedOrgsCount uint
-	Status            string
-	Consortium        ConsortiumDTO
-	Channel           ChannelDTO
-	//InvitationApprovals []*ConsortiumInvitationApproval //HasMany Relationship
-}
-
-// Invitation             ConsortiumInvitation `gorm:"foreignkey:ConsortiumInvitationID"`
-
-type ConsortiumInvitationApprovalDTO struct {
-	ConsortiumInvitationID string
-	OrganizationID         string
-	Status                 string
-	Invitation             InvitationDTO
 }
