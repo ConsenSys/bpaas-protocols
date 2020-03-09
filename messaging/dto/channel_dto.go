77578d0318bfddae03faa55a9f080caa43a0c47a
Barry Earsman <barry@earsman.com>
2020-03-09 16:45:04 +1000

    Removed toDTO functions

--- messaging/dto/channel_dto.go
+++ messaging/dto/channel_dto.go
@@ -15,34 +15,3 @@ type ChannelDTO struct {
 	Public             bool
 	ConsortiumName     string
 }
-
-func ToDTOList(channels []*model.Channel) []*ChannelDTO {
-
-	channelsDTO := []*ChannelDTO{}
-
-	for _, channel := range channels {
-
-		channelsDTO = append(channelsDTO, ToDTO(channel))
-
-	}
-
-	return channelsDTO
-}
-
-func ToDTO(channel *model.Channel) *ChannelDTO {
-
-	return &ChannelDTO{
-		ID:                 channel.ID,
-		Name:               channel.Name,
-		ConsortiumID:       channel.ConsortiumID,
-		OriginalName:       channel.OriginalName,
-		OrderersCount:      uint(len(channel.Orderers)),
-		PeersCount:         uint(len(channel.Peers)),
-		OrganizationsCount: uint(len(channel.Organizations)),
-		ChaincodesCount:    uint(len(channel.Chaincodes)),
-		Status:             channel.Status,
-		Public:             channel.Public,
-		ConsortiumName:     channel.Consortium.Name,
-	}
-
-}
